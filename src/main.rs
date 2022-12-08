2c2b036894e0cfd47570099d85e47302b9f84987
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2022-12-08 21:18:43 +1000

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,25 @@
 mod day01;
+mod day02;
 
-use std::{fs, io};
-use std::io::Read;
+use std::fs;
+use std::io::{Read, stdin};
 
 use clap::{arg, ArgMatches, Command};
 
 fn cli() -> Command {
-    Command::new("aoc")
+    Command::new("advent-of-code-2022")
         .about("Advent of Code 2022")
         .subcommand_required(true)
         .subcommand(
             Command::new("day01")
                 .about("Day 1: Calorie Counting")
-                .arg(arg!(-i --input "The path to the input data.")),
+                .arg(arg!(-i --input <PATH> "The path to the input data.")),
         )
-}
-
-fn get_input(sub_matches: &ArgMatches) -> String {
-
-    // Default to stdin
-    let mut stdin = io::stdin();
-    let mut buffer = String::new();
-
-    // Todo: This locks up if nothing is in stdin
-    if let Ok(buffer_size) = stdin.read_to_string(&mut buffer) {
-        if buffer_size > 0 {
-            return buffer
-        }
-    }
-
-    if let Some(input_path) = sub_matches.get_one::<String>("input") {
-        let input = fs::read_to_string(input_path).expect("Couldn't find input file");
-        return input
-    }
-
-    panic!("Missing input")
+        .subcommand(
+            Command::new("day02")
+                .about("Day 1: Rock Paper Scissors")
+                .arg(arg!(-i --input <PATH> "The path to the input data.")),
+        )
 }
 
 fn main() {
@@ -42,8 +27,13 @@ fn main() {
 
     match matches.subcommand() {
         Some(("day01", sub_matches)) => {
-            let input = get_input(sub_matches);
+            let input = get_input(sub_matches).unwrap();
             day01::run(input)
+        }
+
+        Some(("day02", sub_matches)) => {
+            let input = get_input(sub_matches).unwrap();
+            day02::run(input)
         }
 
         // If all subcommands are defined above, anything else is unreachabe!()
@@ -51,3 +41,21 @@ fn main() {
     }
 }
 
+fn get_input(sub_matches: &ArgMatches) -> Result<String, Box<dyn std::error::Error>> {
+
+    // Try reading from stdin first
+    if atty::isnt(atty::Stream::Stdin) {
+        let mut buffer = String::new();
+        stdin().lock().read_to_string(&mut buffer).unwrap();
+
+        return Ok(buffer)
+    }
+
+    // Otherwise, try using the --input flag
+    if let Some(input_path) = sub_matches.get_one::<String>("input") {
+        let input = fs::read_to_string(input_path).expect("Couldn't find input file");
+        return Ok(input)
+    }
+
+    return Err(Box::from("input was not provided via the --input argument or stdin"))
+}
