ef742263899246c82a4883ae5a2dcfdddbcb2b23
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2022-12-13 16:20:55 +1000

    Add part 2

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 
 pub fn run_part1(input: String) {
 
@@ -8,7 +9,7 @@ pub fn run_part1(input: String) {
         })
         .map(|line: (&str, &str)| -> char {
             let (c1, c2) = line;
-            return find_shared_character(c1, c2).unwrap()
+            return find_shared_character(vec!(c1, c2)).unwrap()
         })
         .map(|ch: char| -> i32 { get_priority(ch).unwrap() })
         .sum();
@@ -16,10 +17,38 @@ pub fn run_part1(input: String) {
     println!("{}", sum)
 }
 
-fn find_shared_character(c1: &str, c2: &str) -> Option<char> {
-    for c in c1.chars() {
-        if c2.contains(c) {
-            return Some(c);
+pub fn run_part2(input: String) {
+
+    let mut total_priority = 0;
+    let lines = input.lines();
+    let total_lines = lines.clone().count();
+    for i in (0..total_lines).step_by(3) {
+        let group_lines = lines.clone().skip(i).take(3).collect();
+        let shared_character = find_shared_character(group_lines).unwrap();
+        let priority = get_priority(shared_character).unwrap();
+        total_priority += priority
+    }
+
+    println!("{}", total_priority)
+}
+
+fn find_shared_character(strings: Vec<&str>) -> Option<char> {
+    let mut map = HashMap::<char, usize>::new();
+
+    for s in &strings {
+        let mut current_str_map = HashMap::<char, usize>::new();
+        for c in s.chars() {
+            if !current_str_map.contains_key(&c){
+                *map.entry(c).or_default() += 1
+            }
+
+            *current_str_map.entry(c).or_default() += 1;
+        }
+    }
+
+    for (character, count) in map {
+        if count == strings.len() {
+            return Some(character);
         }
     }
 
